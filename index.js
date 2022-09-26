e4fa260c1f69e0ec4c15a6300f8fe1594deae008
eze.soruco <eze.soruco93@gmail.com>
2022-09-26 11:51:50 -0300

    CRUD

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const inmuebles = require("./Datos/inmuebles");
+var inmuebles = require("./Datos/inmuebles");
 
 app.use(express.json());
 app.use(cors());
 
 app.get("/api/inmuebles", function (req, res) {
+  res.json(inmuebles);
+});
+
+app.get("/api/inmuebles/:id", function (req, res) {
+  const id = parseInt(req.params.id);
+
+  const inmueble = inmuebles.find((x) => x.id === id);
+
+  res.json(inmueble);
+});
+
+app.put("/api/inmuebles/editar/:id", function (req, res) {
+  const id = parseInt(req.params.id);
+  let inmueble = inmuebles.find((x) => x.id === id);
+
+  inmueble.nombre = req.body.nombre;
+  inmueble.metrosCuadrados = req.body.metrosCuadrados;
+  inmueble.direccion = req.body.direccion;
+  inmueble.precioVenta = req.body.precioVenta;
+
+  res.json(inmueble);
+});
+
+app.delete("/api/inmuebles/eliminar/:id", function (req, res) {
+  const id = parseInt(req.params.id);
+  inmuebles = inmuebles.filter((x) => x.id !== id);
+
   res.json(inmuebles);
 });
 
